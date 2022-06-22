a74f1fdb4443b4a46c58fadcd269ed743dd8d051
lwazilwando <lwazimath10@gmail.com>
2022-06-22 15:23:38 +0200

    init

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,37 @@
-import Component from "react";
+import React, {useState, useEffect} from 'react'
+
 import AddItem from "./addItem";
 import DispalyTransaction from "./DispalyTransaction";
-
+import {BudgetBalance} from './balance'
 
 function Home(props)
 {
 
+    const[expense, setExpense] = useState(0);
+
+    const[income, setIncome] = useState(0);
+
+    useEffect(()=>{
+
+        let expense = 0;
+        let income = 0;
+
+        for(let index = 0; index < props.list.length; index++)
+        {
+            if(props.list[index].transactionType == "Expense"){
+                expense = expense + parseInt(props.list[index].amount);
+            }
+            else{
+                income = income + parseInt(props.list[index].amount);
+            }
+        }
+        setIncome(income);
+        setExpense(expense);
+
+    }, [props.list.length]);
     return(
         <div className="container">
+            <BudgetBalance expense= {expense} income = {income}/>
             <DispalyTransaction list={props.list} />
             <AddItem add={props.add} />
 
